26243f4ba465605b2523afa312387a50444b1739
adriansueve-blip <adriansueve@gmail.com>
2026-06-25 16:42:11 +0200

    Actualizar main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,17 +1,18 @@
 input.onButtonPressed(Button.A, function () {
-    wuKong.setMotorSpeed(wuKong.MotorList.M2, 5)
-    basic.showArrow(ArrowNames.East)
-    basic.pause(500)
+    wuKong.setMotorSpeed(wuKong.MotorList.M1, 5)
+    basic.showArrow(ArrowNames.West)
+    basic.pause(1000)
     wuKong.stopAllMotor()
     basic.clearScreen()
 })
 input.onButtonPressed(Button.B, function () {
-    wuKong.setMotorSpeed(wuKong.MotorList.M2, -5)
-    basic.showArrow(ArrowNames.West)
-    basic.pause(500)
+    wuKong.setMotorSpeed(wuKong.MotorList.M1, -5)
+    basic.showArrow(ArrowNames.East)
+    basic.pause(1000)
     wuKong.stopAllMotor()
     basic.clearScreen()
 })
+basic.showIcon(IconNames.SmallHeart)
 basic.forever(function () {
 	
 })
